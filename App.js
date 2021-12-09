7906eb5ad9b5808d8b3f0780d8a38f08dabd8177
alienor <tykhomoonx@hotmail.com>
2021-12-09 09:27:18 +0100

    gestion clonflits

--- App.js
+++ App.js
@@ -20,17 +20,6 @@ const store = createStore(
 );
 
 // import des pages à inclure dans les navigations
-<<<<<<< HEAD
-import WelcomeScreen from './screens/WelcomeScreen';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import OfferScreen from './screens/OfferScreen';
-import HomeScreen from './screens/HomeScreen';
-import SearchScreen from './screens/SearchScreen';
-import MessagesScreen from './screens/MessagesScreen';
-import ChatScreen from './screens/ChatScreen'
-import CompanyScreen from './screens/CompanyScreen';
-=======
 import WelcomeScreen from "./screens/WelcomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
@@ -39,7 +28,6 @@ import SearchScreen from "./screens/SearchScreen";
 import MessagesScreen from "./screens/MessagesScreen";
 import ChatScreen from "./screens/ChatScreen";
 import CompanyScreen from "./screens/CompanyScreen";
->>>>>>> recherche
 
 // import des modules de navigation
 import { NavigationContainer } from "@react-navigation/native";
