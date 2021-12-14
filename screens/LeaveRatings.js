4b2b2a31a080dcc0d9c78cd0b7ad16473201bbfc
alienor <tykhomoonx@hotmail.com>
2021-12-14 15:33:31 +0100

    pages front & back RatingScreen & LeaveRatings

--- screens/LeaveRatings.js
+++ screens/LeaveRatings.js
@@ -1,83 +1,82 @@
 import React, { useState, useEffect } from 'react';
 import { REACT_APP_IPSERVER } from '@env';
+//Store
+import { connect } from 'react-redux';
 import { View, ScrollView, KeyboardAvoidingView } from 'react-native';
 import { HeaderBar } from '../components/Header'
 import  CompanyCard  from '../components/CompanyCard'
 import { Button, ButtonText } from "../components/Buttons";
-import { Input, AirbnbRating } from 'react-native-elements';
+import { Input, AirbnbRating, Rating } from 'react-native-elements';
 
 
 const LeaveRatingsScreen = (props) => {
 
-    //récupère les infos des inputs
     const [ rate, setRate ] = useState(0);
     const [ feedback, setFeedback ] = useState("");
-    const [ pack, setPackage ] = useState("");
-    const [ details, setDetails ] = useState("");
 
-    // états infos Cie :
     const [ company, setCompany ] = useState(null);
-    const [ companyId, setCompanyId ] = useState(props.route.params && props.route.params.companyId ? props.route.params.companyId : "61b097c526db20ecf9e66953");
-    const [ token, setToken ] = useState("");
-    const [ image, setImage ] = useState(null);
+
+
 
 // useEffect d'initialisation de la page Company :
     useEffect(() => {
 
-  // DANS USE : fonction chargement des infos de la compagnie loggée :
+          // DANS USE : fonction chargement des infos de la compagnie loggée :
           async function loadDataCie() {
               // appel route put pour modifier données company
               var rawDataCie = await fetch(`http://${REACT_APP_IPSERVER}/companies/61b72b8f3ef976a3b8be1b09/token1`); // (`adresseIPserveur/route appelée/req.params?req.query`)
               var dataCie = await rawDataCie.json();
-  console.log("dataCie", dataCie.company);
+// console.log("dataCie", dataCie.company);
               if (dataCie.result) {
                   setCompany(dataCie.company); // set état company avec toutes data
-                  setImage(dataCie.company.companyImage);
-                  setToken(dataCie.company.token);
+                  // setImage(dataCie.company.companyImage);
+                  // setToken(dataCie.company.token);
+                  // setClientId()
               }
           }
           loadDataCie();
 
         }, []);
 
-    var addQuotation = async () => {
-        //reçoit depuis offerpage : offerId, clientId, providerId(companies)
-        const saveReq = await fetch(`http://${REACT_APP_IPSERVER}/quotations/add-quotation`, {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-          body: `clientId=${clientId}&offerId=${offerId}&providerId=${providerId}&date=${new Date()}&sunshine=${sunshine}&area=${area}&pack=${pack}&details=${details}`
-          
-        }) 
-        
-      }
 
-    const quoteRequest = ()=>{
-      props.navigation.navigate("Quotation");
-      addQuotation();
-    }
+    const sendRating = async () => {
+// console.log("dans fonction sendRating");
+        const saveRate = await fetch(`http://${REACT_APP_IPSERVER}/ratings/eY9zt44G4iHEQ2s8YKqJuDUJv0`, { 
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: `feedback=${feedback}&rating=${rate}&dateRating=${new Date()}&clientId=${props.user.companyId}&providerId=${company._id}&userId=${props.user._id}`
+        })
+        var dataRate = await saveRate.json()
+// console.log("dataRate", dataRate);
+        props.navigation.navigate("Rating");
+        setFeedback(dataRate.newRatingSaved.feedback)
+// console.log("dataRate.newRatingSaved.feedback", dataRate.newRatingSaved.feedback);
+// console.log("dans fonction sendRating BIS");
+};
 
-if (company) {
+    if (company) {
 
     return (
     
-    <View style={{flex:1, backgroundColor:"white"}}>
+    <View style={{flex:1}}>
 
         <HeaderBar
           leftComponent
-          title="Laissez votre avis"
+          title="Votre avis"
           navigation={props.navigation}
+          user={props.user}
         >
         </HeaderBar>
 
-            <ScrollView style={{height:500, backgroundColor:"yellow"}}>
+            <ScrollView style={{backgroundColor:"white"}}>
 
-            {/* <KeyboardAvoidingView 
+            <KeyboardAvoidingView 
                 behavior="position" 
                 // contentContainerStyle={{alignItems: "center", paddingLeft:20, paddingRight: 20}}
             >
-            </KeyboardAvoidingView> */}
+            </KeyboardAvoidingView>
 
-            <View style={{flex:1, paddingBottom:80}}>
+            <View style={{flex:1, paddingBottom:80, backgroundColor:"white"}}>
                 <View style={{ top:10, paddingLeft:15, paddingRight: 15}}>
                 <CompanyCard
                     navigation={props.navigation}
@@ -85,46 +84,40 @@ if (company) {
                 />
                 </View>
 
-                <View style={{backgroundColor:"pink", top:30, paddingLeft:15, paddingRight: 15}}>
+                <View style={{top:40, paddingLeft:15, paddingRight: 15}}>
                 <Input
                   value={feedback}
                   label={`Décrivez votre expérience avec ${company.companyName}`}
                   // keyboardType="numeric"
                   placeholder='En quelques mots'
-                  onChange={(e) => setFeedback(e.target.value)}
-                />
-                <Input 
-                    value={rate}
-                    label={`Notez votre expérience avec ${company.companyName}`}
-                    keyboardType="numeric"
-                    labelStyle={{marginTop: 10}}
-                    placeholder='Donnez une note de 1 à 10'
-                    onChange={(e) => setRate(e.target.value)}
+                  onChangeText={(value) => setFeedback(value)}
                 />
                 </View>
 
-                <View style={{backgroundColor:"green", height:50, justifyContent:"center", top:50, paddingLeft:15, paddingRight: 15}}>
+                <View style={{height:50, justifyContent:"center", top:50, paddingLeft:15, paddingRight: 15}}>
                 <AirbnbRating
                     type="custom"
                     selectedColor="#F47805"
                     unSelectedColor="#F4780533"
                     reviewColor="#F47805"
-                    defaultRating={3} //changer avec rating
-                    isDisabled
+                    defaultRating={0}
                     count={5}
-                    size={20}
-                  showRating={false}
+                    size={30}
+                    showRating={false}
+                    onFinishRating={(rating) => setRate(rating)}
                 />
                 </View>
 
-                <View style={{backgroundColor:"blue", top:50, paddingLeft:15, paddingRight: 15}}>
+                <View style={{top:50, paddingHorizontal:15, paddingVertical:20, alignItems:"center"}}>
+                  <View style={{paddingBottom:10}}>
                   <Button 
                       title="Poster l'avis"
                       size="md"
                       color="primary"
-                      onPress={() => quoteRequest()}
+                      onPress={() => sendRating()}
                   >
                   </Button>
+                  </View>
                   <ButtonText 
                     title="Annuler" 
                     onPress={() => props.navigation.goBack()}
@@ -133,7 +126,7 @@ if (company) {
                 </View>
 
             </View>
-
+            
             </ScrollView>
 
     </View>
@@ -143,6 +136,11 @@ if (company) {
               }
 };
 
+// on récupère le user stocké dans le store : 
+function mapStateToProps(state) {
+    return { user: state.user }
+  };
 
-export default LeaveRatingsScreen;
+export default connect(mapStateToProps, null)(LeaveRatingsScreen);
+
 
